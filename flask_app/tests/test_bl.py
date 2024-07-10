20ad1197904d9356af3c643a16c4182ef5c6db6b
Jimmy Shultz <jimmyshultz3@gmail.com>
2024-07-10 18:28:43 -0500

    Add missing test cases for the BL class

--- flask_app/tests/test_bl.py
+++ flask_app/tests/test_bl.py
@@ -1,41 +1,39 @@
 
 """
-    A class to test the DAL class methods.
+    A class to test the BusinessLogic class methods.
 
-    Some tests use the test data that is included in the database 
-    creation scripts.
-
-    Attributes
+    Fixtures
     ----------
-    dal : DAL
-        An instance of the DAL class.
+    - dal_mock - Mocks the Data Access Layer
+    - app - Creates the Flask app with a mocked DAL
+    - setup (in TestBL class) - Setup for each test method
     
     Tested Methods
     --------------
+    - check_recipe
+    - get_cookbook_names
+    - get_cookbook_info
+    - get_all_recipe_names
+    - get_recipe_info
+    - add_cookbook
+    - delete_cookbook
+    - add_recipe
+    - delete_recipe
+    - add_ingredient
+    - delete_ingredient
+    - add_ingredient_recipe_pairing
+    - add_user
+    - login_user
     
     Untested Methods
     ----------------
-    - check_recipe - not implemented
-    - get_cookbook_names - 
-    - get_cookbook_info - 
-    - get_recipe_names - 
-    - get_all_recipe_names - 
-    - get_recipe_info - 
-    - add_cookbook - 
-    - delete_cookbook - 
-    - add_recipe - 
-    - delete_recipe - 
-    - add_ingredient - 
-    - delete_ingredient - 
-    - add_ingredient_recipe_pairing - 
-    - add_user - 
-    - login_user - 
+    - get_recipe_names - need to tie recipes to a cookbook in mock data
     
 """
 
 import pytest, sys, os
 from unittest.mock import MagicMock
-from flask import Flask, session, jsonify
+from werkzeug.security import generate_password_hash
 
 # Add the backend directory to the system path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../backend')))
@@ -49,6 +47,9 @@ def dal_mock():
     mock.get_recipe_names.return_value = ['recipe1', 'recipe2']
     mock.get_cookbook_names.return_value = ['cookbook1', 'cookbook2']
     mock.get_cookbook_info.return_value = ('Cookbook1', 1, 'http://example.com')
+    mock.get_recipe_info.return_value = ('Recipe1', 'Cookbook1', 2, 1, 'http://example.com', 1)
+    mock.get_recipe_ingredients.return_value = ['ingredient1', 'ingredient2']
+    mock.get_user_info.return_value = (1, 'test@example.com', generate_password_hash('test'), 'test', 'test')
     return mock
 
 @pytest.fixture
@@ -104,3 +105,93 @@ class TestBL:
             'recipes': ['recipe1', 'recipe2']
         }
         assert response.json == expected_response
+
+    def test_get_cookbook_info_invalid(self):
+        with self.app.session_transaction() as sess:
+            sess['user_id'] = 1
+        response = self.app.get('/cookbook_info/invalid_cookbook')
+        assert response.status_code == 200
+        assert response.json == { 'validity' : False, 'cookbook_name': '', 'online': False, 'message': '', 'url': '', 'recipes': [] }
+    
+    def test_get_all_recipe_names(self):
+        with self.app.session_transaction() as sess:
+            sess['user_id'] = 1
+        response = self.app.get('/all_recipe_names')
+        assert response.status_code == 200
+        assert response.json == ['recipe1', 'recipe2']
+
+    def test_get_recipe_info(self):
+        with self.app.session_transaction() as sess:
+            sess['user_id'] = 1
+        response = self.app.get('/recipe_info/recipe1')
+        assert response.status_code == 200
+        expected_response = {
+            'recipe_name': 'Recipe1',
+            'cookbook_name': 'Cookbook1',
+            'servings': 2,
+            'is_online': 1,
+            'url': 'http://example.com',
+            'ingredients': ['ingredient1', 'ingredient2']
+        }
+        assert response.json == expected_response
+
+    def test_add_cookbook(self):
+        with self.app.session_transaction() as sess:
+            sess['user_id'] = 1
+        response = self.app.put('/add_cookbook', json={'new_cookbook_name': 'NewCookbook', 'new_website': 'http://example.com/', 'new_is_book': True})
+        assert response.status_code == 200
+        assert response.json == {'message': f'Cookbook NewCookbook added successfully'}
+
+    def test_delete_cookbook(self):
+        with self.app.session_transaction() as sess:
+            sess['user_id'] = 1
+        response = self.app.delete('/delete_cookbook/NewCookbook')
+        assert response.status_code == 200
+        assert response.json == {'message': f'Cookbook NewCookbook deleted successfully'}
+
+    def test_add_recipe(self):
+        with self.app.session_transaction() as sess:
+            sess['user_id'] = 1
+        response = self.app.put('/add_recipe', json={'new_recipe_name': 'NewRecipe', 'new_cookbook_name': 'Cookbook1', 'new_servings': 2, 'new_is_online': 1, 'new_webpage': 'http://example.com'})
+        assert response.status_code == 200
+        assert response.json == {'message': f'Recipe NewRecipe added successfully'}
+
+    def test_delete_recipe(self):
+        with self.app.session_transaction() as sess:
+            sess['user_id'] = 1
+        response = self.app.delete('/delete_recipe/NewRecipe')
+        assert response.status_code == 200
+        assert response.json == {'message': f'Recipe NewRecipe deleted successfully'}
+    
+    def test_add_ingredient(self):
+        with self.app.session_transaction() as sess:
+            sess['user_id'] = 1
+        response = self.app.put('/add_ingredient', json={'new_ingredient': 'NewIngredient'})
+        assert response.status_code == 200
+        assert response.json == {'message': f'Ingredient NewIngredient added successfully'}
+
+    def test_delete_ingredient(self):
+        with self.app.session_transaction() as sess:
+            sess['user_id'] = 1
+        response = self.app.delete('/delete_ingredient/NewIngredient')
+        assert response.status_code == 200
+        assert response.json == {'message': f'Ingredient NewIngredient deleted successfully'}
+
+    def test_add_ingredient_recipe_pairing(self):
+        with self.app.session_transaction() as sess:
+            sess['user_id'] = 1
+        response = self.app.put('/add_ingredient_recipe_pairing', json={'recipe_name': 'Recipe1', 'ingredient_name': 'ingredient1'})
+        assert response.status_code == 200
+        assert response.json == {'message': f'Ingredient ingredient1 paired with recipe Recipe1 successfully'}
+
+    def test_add_user(self):
+        response = self.app.put('/add_user', json={'email': 'test@example.com', 'password': 'test', 'first_name': 'test', 'last_name': 'test'})
+        assert response.status_code == 200
+        assert response.json == {'message': f'User test@example.com added successfully', 'success': True}
+    
+    def test_login_user(self):
+        response = self.app.post('/login', json={'email': 'test@example.com', 'password': 'test'})
+        assert response.json == {'message': f'User test@example.com logged in successfully', 'email': 'test@example.com', 'first_name': 'test', 'last_name': 'test', 'success': True}
+        assert response.status_code == 200
+
+    
